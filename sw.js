fd5dddae35850a053dc4aff019317412d3dffb24
Safarifarm <bibimukrema0@gmail.com>
2026-08-14 08:36:24 +0530

    dssdgb

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='safari-farm-disabled-v9'; const ASSETS=[];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
